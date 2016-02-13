08ca978f194196e53f00beea2bbfe866e2ac30f6
qijunbo <junboqi@foxmail.com>
2016-02-13 11:33:54 +0800

    this refreshController can not works will with auditControllers,  because they are not in the same life cycle, and they want manipulate the same $scope.  Currently, I cannot find a better solution  than merging  the two controllers into one controller.

--- src/main/webapp/js/auditControllers.js
+++ src/main/webapp/js/auditControllers.js
@@ -4,12 +4,14 @@
 
 var auditControllers = angular.module('auditControllers', []);
 
-auditControllers.controller('auditController', [ '$scope',  '$routeParams', 'auditService', 'chargePointSerialService',
-		function auditController($scope, $routeParams, auditService, chargePointSerialService) {
+auditControllers.controller('auditController', [ '$scope',  '$routeParams', 'auditService', 'chargePointSerialService',  '$timeout', 'auditmoreService',
+		function auditController($scope, $routeParams, auditService, chargePointSerialService, $timeout, auditmoreService) {
 	
 			$scope.audits = [];
 			$scope.deviceSerial = $routeParams.serial;
 			console.log($routeParams.serial);
+
+			//get the current charge point information by deviceSerial.
 			chargePointSerialService.get({serial: $routeParams.serial }, {}, function success(response) {
 				//console.log("get ChargePoint Success:" + JSON.stringify(response));
 				$scope.chargepoint = response;
@@ -19,6 +21,7 @@ auditControllers.controller('auditController', [ '$scope',  '$routeParams', 'aud
 				console.log("Error:" + JSON.stringify(errorResponse));
 			});
 
+			//get the log information of current charge point.
 			auditService.get({serial: $routeParams.serial }, {}, function success(response) {
 				//console.log("list audit Success:" + JSON.stringify(response));
 				$scope.audits = response;
@@ -27,6 +30,47 @@ auditControllers.controller('auditController', [ '$scope',  '$routeParams', 'aud
 				alert("Connot connect to server.");
 				console.log("Error:" + JSON.stringify(errorResponse));
 			});
+
+			// periodically call the API to get the recent logs if any.
+			var updateClock = function() {
+
+				var length = $scope.audits.length;
+				//console.log("Audit:" + JSON.stringify( $scope.audits[0]  ));
+		 		//console.log($scope.deviceSerial);
+		 		var time = 0;
+				console.log( "time " + time );
+				//console.log(JSON.stringify($scope.audits));
+				if( length > 0 ){
+					time = $scope.audits[0].time; 
+					//console.log(time);
+					if(time == null){
+						time = 0;
+					}
+				}
+				//var formatedDate = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' +  time.getDate()  + ' ' + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds ();
+				
+				auditmoreService.get({serial: $scope.deviceSerial,  date: time  }, {}, function success(response) {
+					//console.log("refresh audit Success:" + JSON.stringify(response));
+					console.log("response array length: " + response.length);
+					if(response.length > 1){
+						$scope.audits =  $scope.audits.concat( response);
+					}
+					if( $scope.audits.length > 50 ){
+						$scope.audits = $scope.audits.slice(0, 49);
+					}
+					//console.log($scope.audits.length);
+				}, function error(errorResponse) {
+					//alert("Connot connect to server.");
+					console.log("Error:" + JSON.stringify(errorResponse));
+				});
+				 
+				$timeout(function() {
+					updateClock();
+				}, 3000);
+			};
+
+			updateClock();
+
 
 			$scope.deleteLogs = function(){
 				//alert($routeParams.serial);
@@ -48,7 +92,12 @@ auditControllers.controller('auditController', [ '$scope',  '$routeParams', 'aud
 
 
  
+/** this refreshController can not works will with auditControllers, 
+ *  because they are not in the same life cycle, and they want manipulate the same $scope.
+ *  Currently, I cannot find a better solution  than merge the tow controllers into one controller.
+ */
 
+/*
 auditControllers.controller('refreshController', [ '$scope',  '$timeout', 'auditmoreService',
 		function refreshController($scope, $timeout, auditmoreService) {
 			var updateClock = function() {
@@ -90,3 +139,5 @@ auditControllers.controller('refreshController', [ '$scope',  '$timeout', 'audit
 			updateClock();
 
 		} ]);
+
+*/
